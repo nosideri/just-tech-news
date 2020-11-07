a970b651b98ca9a2f6399a20f83f00afa221bfea
nosideri <madi.stollmeyer@gmail.com>
2020-11-07 12:43:22 -0800

    added post routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const User = require('./User');
 const Post = require("./Post");
 
+// user can have many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// but post belongs to one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
 module.exports = { User };
